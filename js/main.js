7f9d6943bc4c800d7d3852c6009e5a7b088aefd8
aron-alvarenga <aron.alvarenga@hotmail.com>
2023-05-27 11:34:00 -0400

    Outputting Lists

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ new Vue({
   el: "#app",
   data: {
     name: "Aron",
+    elements: [],
   },
   methods: {
     changeName: function () {
       this.name = "Alvarenga";
+    },
+    addElement: function () {
+      this.elements.push(this.elements.length + 1);
     },
   },
 });
