8f085d7d4e46656cd6733b6c7dfaf78bf25c00d3
aron-alvarenga <aron.alvarenga@hotmail.com>
2023-05-27 11:58:06 -0400

    Styling Elements Dynamically

--- js/main.js
+++ js/main.js
@@ -11,5 +11,8 @@ new Vue({
     addElement: function () {
       this.elements.push(this.elements.length + 1);
     },
+    getColor: function (number) {
+      return number % 2 == 0 ? "lime" : "tomato";
+    },
   },
 });
